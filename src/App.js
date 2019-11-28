92d39fc0dc9b1520ff2020c3569da1d361d0b8f8
julianosbarbosa <julianoestudos@gmail.com>
2019-11-28 18:41:56 -0300

    UPDATE - Create Routers

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
 import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import MainPage from "./pages/main";
+import Login from "./pages/login";
 
-const App = () => <h1>App</h1>;
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact component={MainPage} />
+        <Route path="/login" component={Login} />
+        <Route />
+      </Switch>
+    </BrowserRouter>
+  );
+};
 
 export default App;
