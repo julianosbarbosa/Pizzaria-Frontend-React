0d50773b122b5302e303529fa3fad17340ab22b8
julianosbarbosa <julianoestudos@gmail.com>
2019-12-03 17:29:06 -0300

    UPDATE - social login connection with firebase

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,21 +1,59 @@
-import React from "react";
+import React, { Component } from "react";
 import Logo from "../../assets/logotipo.png";
 import { Button, Grid } from "@material-ui/core";
 import { Container } from "./styles";
+import * as firebase from "firebase";
+import "firebase/auth";
 
-export default function Login() {
-  return (
-    <Container>
-      <Grid container direction="column" alignItems="center" spacing={40}>
-        <Grid item>
-          <img src={Logo} alt="logotipo" />
+var firebaseConfig = {
+  apiKey: "AIzaSyC5WPL9V3JFRnk2k5WmcmgLIP5XTB8wHcU",
+  authDomain: "pizzaria-60905.firebaseapp.com",
+  databaseURL: "https://pizzaria-60905.firebaseio.com",
+  projectId: "pizzaria-60905",
+  storageBucket: "pizzaria-60905.appspot.com",
+  messagingSenderId: "255692066413",
+  appId: "1:255692066413:web:830539d1a2651582b5b964",
+  measurementId: "G-41LC8QQLHD"
+};
+
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
+class Login extends Component {
+  componentDidMount() {
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        console.log("usuario logado ", user);
+      } else {
+        console.log("usuario não logado ", user);
+      }
+    });
+  }
+
+  render() {
+    return (
+      <Container>
+        <Grid container direction="column" alignItems="center" spacing={4}>
+          <Grid item>
+            <img src={Logo} alt="logotipo" />
+          </Grid>
+          <Grid item>
+            <Button
+              variant="outlined"
+              color="primary"
+              fullWidth
+              onClick={() => {
+                const provider = new firebase.auth.GoogleAuthProvider();
+                firebase.auth().signInWithRedirect(provider);
+              }}
+            >
+              Entrar com Google
+            </Button>
+          </Grid>
         </Grid>
-        <Grid item>
-          <Button variant="outlined" color="primary" fullWidth>
-            Entrar com GitHub
-          </Button>
-        </Grid>
-      </Grid>
-    </Container>
-  );
+      </Container>
+    );
+  }
 }
+
+export default Login;
