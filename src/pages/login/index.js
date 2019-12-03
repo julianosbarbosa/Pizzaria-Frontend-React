8ada5fa21c702f65416ed3654476ef855551b669
julianosbarbosa <julianoestudos@gmail.com>
2019-12-02 23:05:47 -0300

    UPDATE - social login page

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,7 +1,21 @@
 import React from "react";
-
-// import { Container } from './styles';
+import Logo from "../../assets/logotipo.png";
+import { Button, Grid } from "@material-ui/core";
+import { Container } from "./styles";
 
 export default function Login() {
-  return <h1>Login</h1>;
+  return (
+    <Container>
+      <Grid container direction="column" alignItems="center" spacing={40}>
+        <Grid item>
+          <img src={Logo} alt="logotipo" />
+        </Grid>
+        <Grid item>
+          <Button variant="outlined" color="primary" fullWidth>
+            Entrar com GitHub
+          </Button>
+        </Grid>
+      </Grid>
+    </Container>
+  );
 }
---
+++ src/pages/login/styles.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  padding: 20px;
+  img {
+    width: 96px;
+  }
+
+  button {
+    font-size: 16px;
+    padding: 9px;
+  }
+`;
