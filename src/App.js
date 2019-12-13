9f767b26e615cb46c7a4b228351ebfd183048b4f
julianosbarbosa <julianoestudos@gmail.com>
2019-12-13 14:58:38 -0300

    UPDATE - Lazy Loading

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
-import React, { Fragment } from "react";
+import React, { lazy, Suspense, Fragment } from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
-import MainPage from "./pages/main";
-import Login from "./pages/login";
-import { CssBaseline } from "@material-ui/core";
+import { CssBaseline, LinearProgress } from "@material-ui/core";
+
+const MainPage = lazy(() => import("./pages/main"));
+const Login = lazy(() => import("./pages/login"));
 
 const App = () => {
   return (
     <Fragment>
       <CssBaseline />
       <BrowserRouter>
-        <Switch>
-          <Route path="/login" component={Login} />
-          <Route component={MainPage} />
-        </Switch>
+        <Suspense fallback={<LinearProgress />}>
+          <Switch>
+            <Route path="/login" component={Login} />
+            <Route component={MainPage} />
+          </Switch>
+        </Suspense>
       </BrowserRouter>
     </Fragment>
   );
